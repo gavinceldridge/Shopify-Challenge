273374e6cf80b66ff23586a34e206a5d196222ab
Gavin Eldridge <gchacka@gmail.com>
2022-05-13 07:03:55 -0700

    push to surge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,40 +6,55 @@ import AiForm from "./AiForm";
 import Responses from "./Responses";
 
 function App() {
+	// console.log(process.env.REACT_APP_OPEN_AI_SK);
+
 	const configuration = new Configuration({
-		organization: "org-pp0wWrYEeCmUJsgTrkk7ub6f",
-		apiKey: "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",
+		organization: process.env.REACT_APP_OPEN_AI_ORG,
+		apiKey: process.env.REACT_APP_OPEN_AI_SK,
 	});
 
 	const [input, setInput] = useState("");
-  const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
+	const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
 
-	const openai = new OpenAIApi(configuration);
 	const askAi = async (length) => {
-		await openai.listEngines();
-
-    console.log(input);
+    const openai = new OpenAIApi(configuration);
+		const engines = await openai.listEngines();
+		console.log(engines);
+		console.log(input);
 		if (input.length === 0) return;
-
-		const response = await openai.createCompletion("text-curie-001", {
-			prompt: input,
-			max_tokens: length,
-		});
-    console.log(response.data);
-		return response.data.choices;
+		try {
+			const response = await openai.createCompletion("text-curie-001", {
+				prompt: input,
+				max_tokens: length,
+			});
+			console.log(response.data);
+			return response.data.choices;
+		} catch (e) {
+			console.log(e);
+			return false;
+		}
 	};
 
 	const handleSubmit = async (e, length) => {
 		e.preventDefault();
-    const res = await askAi(length);
-    console.log(res);
-    setQuestionsAndAnswers([{question: input, answer: res[0].text}, ...questionsAndAnswers]);
+		const res = await askAi(length);
+		if (res) {
+			console.log(res);
+			setQuestionsAndAnswers([
+				{ question: input, answer: res[0].text },
+				...questionsAndAnswers,
+			]);
+		}
 	};
 
 	return (
 		<div className="App">
-			<AiForm input={input} setInput={setInput} handleSubmit={handleSubmit} />
-      <Responses questionsAndAnswers = {questionsAndAnswers} />
+			<AiForm
+				input={input}
+				setInput={setInput}
+				handleSubmit={handleSubmit}
+			/>
+			<Responses questionsAndAnswers={questionsAndAnswers} />
 		</div>
 	);
 }
