248254c8c6457ea72a28274935cac8c247141789
Gavin Eldridge <gchacka@gmail.com>
2022-05-16 17:17:31 -0700

    got rid of a warning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
 
 import useLocalStorage from "./useLocalStorage";
